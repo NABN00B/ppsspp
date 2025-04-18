112f11a748463a5629479a5f0204fc3ec078f33c
Nab <22328803+NABN00B@users.noreply.github.com>
2025-04-18 20:56:21 +0000

    improvement

--- UI/DebugOverlay.cpp
+++ UI/DebugOverlay.cpp
@@ -457,56 +457,36 @@ void DrawFPS(UIContext *ctx, const Bounds &bounds) {
 	char temp[256];
 	StringWriter w(temp);
 
+	ctx->Flush();
+	ctx->BindFontTexture();
+	ctx->Draw()->SetFontScale(0.7f, 0.7f);
+
+	int lines_drawn = 0;
+
 	if ((g_Config.iShowStatusFlags & ((int)ShowStatusFlags::FPS_COUNTER | (int)ShowStatusFlags::SPEED_COUNTER)) == ((int)ShowStatusFlags::FPS_COUNTER | (int)ShowStatusFlags::SPEED_COUNTER)) {
 		// Both at the same time gets a shorter formulation.
 		w.F("%2.0f/%2.0f (%5.1f%%)", actual_fps, fps, vps / ((g_Config.iDisplayRefreshRate / 60.0f * 59.94f) / 100.0f));
+		ctx->Draw()->DrawTextShadow(ubuntu24, w.as_view(), bounds.x2() - 10, lines_drawn++ * 10 + 9, 0xFF3FFF3F, ALIGN_TOPRIGHT | FLAG_DYNAMIC_ASCII);
 	} else {
 		if (g_Config.iShowStatusFlags & (int)ShowStatusFlags::FPS_COUNTER) {
-			w.F("%5.1f FPS", actual_fps);
+			w.F("%4.1f FPS", actual_fps);
+			ctx->Draw()->DrawTextShadow(ubuntu24, w.as_view(), bounds.x2() - 10, lines_drawn++ * 10 + 9, 0xFF3FFF3F, ALIGN_TOPRIGHT | FLAG_DYNAMIC_ASCII);
 		} else if (g_Config.iShowStatusFlags & (int)ShowStatusFlags::SPEED_COUNTER) {
 			w.F("%5.1f%%", vps / (59.94f / 100.0f));
+			ctx->Draw()->DrawTextShadow(ubuntu24, w.as_view(), bounds.x2() - 10, lines_drawn++ * 10 + 9, 0xFF3FFF3F, ALIGN_TOPRIGHT | FLAG_DYNAMIC_ASCII);
 		}
 	}
 	if (System_GetPropertyBool(SYSPROP_CAN_READ_BATTERY_PERCENTAGE)) {
 		if (g_Config.iShowStatusFlags & (int)ShowStatusFlags::BATTERY_PERCENT) {
 			const int percentage = System_GetPropertyInt(SYSPROP_BATTERY_PERCENTAGE);
 			// Just plain append battery. Add linebreak?
-			w.F("\n%3d%%", percentage);
+			w.F("%3d%%", percentage);
+			ctx->Draw()->DrawTextShadow(ubuntu24, w.as_view(), bounds.x2() - 10, lines_drawn++ * 10 + 9, 0xFF3FFF3F, ALIGN_TOPRIGHT | FLAG_DYNAMIC_ASCII);
 		}
 	}
 
-	ctx->Flush();
-	/*w.F("\n%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c",
-		0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87,
-		0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F);
-	w.F("\n%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c",
-		0x90, 0x91, 0x92, 0x93, 0x94, 0x95, 0x96, 0x97,
-		0x98, 0x99, 0x9A, 0x9B, 0x9C, 0x9D, 0x9E, 0x9F);
-	w.F("\n%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c",
-		0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7,
-		0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF);
-	w.F("\n%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c",
-		0xB0, 0xB1, 0xB2, 0xB3, 0xB4, 0xB5, 0xB6, 0xB7,
-		0xB8, 0xB9, 0xBA, 0xBB, 0xBC, 0xBD, 0xBE, 0xBF);
-	/*w.F("\n%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c",
-		0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7,
-		0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF);
-	w.F("\n%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c",
-		0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0xD5, 0xD6, 0xD7,
-		0xD8, 0xD9, 0xDA, 0xDB, 0xDC, 0xDD, 0xDE, 0xDF);
-	w.F("\n%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c",
-		0xE0, 0xE1, 0xE2, 0xE3, 0xE4, 0xE5, 0xE6, 0xE7,
-		0xE8, 0xE9, 0xEA, 0xEB, 0xEC, 0xED, 0xEE, 0xEF);
-	w.F("\n%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c",
-		0xF0, 0xF1, 0xF2, 0xF3, 0xF4, 0xF5, 0xF6, 0xF7,
-		0xF8, 0xF9, 0xFA, 0xFB, 0xFC, 0xFD, 0xFE, 0xFF)*/;
-
-	ctx->Flush();
-
-	ctx->BindFontTexture();
-	ctx->Draw()->SetFontScale(0.7f, 0.7f);
-	ctx->Draw()->DrawText(ubuntu24, w.as_view(), bounds.x2() - 8, 20, 0xc0000000, ALIGN_TOPRIGHT | FLAG_DYNAMIC_ASCII);
-	ctx->Draw()->DrawText(ubuntu24, w.as_view(), bounds.x2() - 10, 19, 0xFF3fFF3f, ALIGN_TOPRIGHT | FLAG_DYNAMIC_ASCII);
+	/*ctx->Draw()->DrawText(ubuntu24, w.as_view(), bounds.x2() - 8, 20, 0xc0000000, ALIGN_TOPRIGHT | FLAG_DYNAMIC_ASCII);
+	ctx->Draw()->DrawText(ubuntu24, w.as_view(), bounds.x2() - 10, 19, 0xFF3fFF3f, ALIGN_TOPRIGHT | FLAG_DYNAMIC_ASCII);*/
 	ctx->Draw()->SetFontScale(1.0f, 1.0f);
 	ctx->Flush();
 	ctx->RebindTexture();
