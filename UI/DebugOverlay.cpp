e65c585f9bd1268ae3c0b383c1b11cda9aac9b9c
Nab <22328803+NABN00B@users.noreply.github.com>
2025-04-19 09:19:59 +0000

    build fix

--- UI/DebugOverlay.cpp
+++ UI/DebugOverlay.cpp
@@ -489,10 +489,10 @@ void DrawFPS(UIContext *ctx, const Bounds &bounds) {
 		}
 	}
 
-	ctx->Draw()->DrawTextShadow(ubuntu24, fpsbuf+"[\u2007\u2007\u2007]", bounds.x2() - 10, lines_drawn++ * 25 + 0, 0xFF3FFF3F, ALIGN_TOPRIGHT | FLAG_DYNAMIC_ASCII);
-	ctx->Draw()->DrawTextShadow(ubuntu24, fpsbuf+"[#\u2007\u2007]", bounds.x2() - 10, lines_drawn++ * 25 + 0, 0xFF3FFF3F, ALIGN_TOPRIGHT | FLAG_DYNAMIC_ASCII);
-	ctx->Draw()->DrawTextShadow(ubuntu24, fpsbuf+"[##\u2007]", bounds.x2() - 10, lines_drawn++ * 25 + 0, 0xFF3FFF3F, ALIGN_TOPRIGHT | FLAG_DYNAMIC_ASCII);
-	ctx->Draw()->DrawTextShadow(ubuntu24, fpsbuf+"[###]", bounds.x2() - 10, lines_drawn++ * 25 + 0, 0xFF3FFF3F, ALIGN_TOPRIGHT | FLAG_DYNAMIC_ASCII);
+	ctx->Draw()->DrawTextShadow(ubuntu24, strcat(fpsbuf, "[\u2007\u2007\u2007]"), bounds.x2() - 10, lines_drawn++ * 25 + 0, 0xFF3FFF3F, ALIGN_TOPRIGHT | FLAG_DYNAMIC_ASCII);
+	ctx->Draw()->DrawTextShadow(ubuntu24, strcat(fpsbuf, "[#\u2007\u2007]"), bounds.x2() - 10, lines_drawn++ * 25 + 0, 0xFF3FFF3F, ALIGN_TOPRIGHT | FLAG_DYNAMIC_ASCII);
+	ctx->Draw()->DrawTextShadow(ubuntu24, strcat(fpsbuf, "[##\u2007]"), bounds.x2() - 10, lines_drawn++ * 25 + 0, 0xFF3FFF3F, ALIGN_TOPRIGHT | FLAG_DYNAMIC_ASCII);
+	ctx->Draw()->DrawTextShadow(ubuntu24, strcat(fpsbuf, "[###]"), bounds.x2() - 10, lines_drawn++ * 25 + 0, 0xFF3FFF3F, ALIGN_TOPRIGHT | FLAG_DYNAMIC_ASCII);
 
 	/*ctx->Draw()->DrawText(ubuntu24, w.as_view(), bounds.x2() - 8, 20, 0xc0000000, ALIGN_TOPRIGHT | FLAG_DYNAMIC_ASCII);
 	ctx->Draw()->DrawText(ubuntu24, w.as_view(), bounds.x2() - 10, 19, 0xFF3fFF3f, ALIGN_TOPRIGHT | FLAG_DYNAMIC_ASCII);*/
